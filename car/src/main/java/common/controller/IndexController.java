7a59d51cb85989fc58a6da1a6963c533ccb3f408
DevJeongWS <163420205+DevJeongWS@users.noreply.github.com>
2024-05-10 11:56:20 +0900

    semi project [car] create

---
+++ car/src/main/java/common/controller/IndexController.java
@@ -0,0 +1,18 @@
+package common.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class IndexController extends AbstractController {
+	
+	public IndexController() {
+	}
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// super.setRedirect(false); // default 값이 false라 굳이 안해주어도 된다.
+		super.setViewPage("/WEB-INF/index.jsp");
+		
+	}
+}

a9c0308a4cdb468b1630c9ec6afaedc5a51cdf27
jeong-useog <jeong-useog@218.38.137.28>
2024-05-17 22:02:33 +0900

    ㅁ

--- car/src/main/java/common/controller/FrontController.java
+++ car/src/main/java/common/controller/FrontController.java
@@ -22,7 +22,7 @@ import java.util.Properties;
 	      description = "사용자가 웹에서 *.car 을 했을 경우 이 서블릿이 응답을 해주도록 한다.",
 	      urlPatterns = {"*.car"},
 	      initParams = { 
-	      @WebInitParam(name = "propertyConfig", value = "C:\\git\\car\\car\\src\\main\\webapp\\WEB-INF\\Command.properties", description = "*.car 에 대한 클래스의 매핑파일") 
+	      @WebInitParam(name = "propertyConfig", value = "/Users/jeong-useog/Desktop/NCS/workspace_JSP/car/car/src/main/webapp/WEB-INF/Command.properties", description = "*.car 에 대한 클래스의 매핑파일") 
 	      })
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;

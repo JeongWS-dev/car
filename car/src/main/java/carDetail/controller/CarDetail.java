8cf4eebaaee136fb7e7101d21fe9ab58c8e58ee1
kimdh <user@DESKTOP-TB43R6C>
2024-05-23 10:02:24 +0900

    CarDetail 수정

--- car/src/main/java/carDetail/controller/CarDetail.java
+++ car/src/main/java/carDetail/controller/CarDetail.java
@@ -4,10 +4,11 @@ import common.controller.AbstractController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class G90Black extends AbstractController {
+public class CarDetail extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
 		super.setViewPage("/WEB-INF/carDetail/g90Black.jsp");
 
 	}

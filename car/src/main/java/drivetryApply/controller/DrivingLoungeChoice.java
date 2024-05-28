8f5373b766e59fe35ee3016c00c001c976788698
kimdh <user@DESKTOP-TB43R6C>
2024-05-28 14:02:35 +0900

    SDF

--- car/src/main/java/drivetryApply/controller/DrivingLoungeChoice.java
+++ car/src/main/java/drivetryApply/controller/DrivingLoungeChoice.java
@@ -14,36 +14,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class DrivingLoungeChoice extends AbstractController {
-	
-	private CarDAO_kimdh cdao = null;
-	
-	public DrivingLoungeChoice() {
-		cdao = new CarDAO_imple_kimdh();
-	}
-	
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		String drivingLounge = request.getParameter("drivingLounge");
-		
-		
-		// 드라이빙 라운지 조회하기
-		Map<String, String> carList = cdao.dlSearch(drivingLounge);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/drivetryApply/drivingLoungeChoice.jsp");	
+   
+   private CarDAO_kimdh cdao = null;
+   
+   public DrivingLoungeChoice() {
+      cdao = new CarDAO_imple_kimdh();
+   }
+   
+   @Override
+   public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+      // 해당 페이지에서는 무조건 modal창이 뜨도록 --> 차량 선택을 해라
+		String method = request.getMethod();
+	    
+	// 모달에 띄울 메세지
+		  String message = "현재 진행중인 시승 신청 단계를 완료해야\r\n"
+  		  		+ "다음 단계로 이동이 가능합니다.";  
 
-	}
+      
+      
+      super.setRedirect(false);
+      super.setViewPage("/WEB-INF/drivetryApply/drivingLoungeChoice.jsp");   
+
+   }
 
 }

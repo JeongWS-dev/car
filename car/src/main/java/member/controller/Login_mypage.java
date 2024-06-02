44b8ee16f8298bd6c36f28f676b2fc698c0a425d
1up0 <152974341+1up0@users.noreply.github.com>
2024-06-02 16:31:24 +0900

    마이페이지 자바 업로드

---
+++ car/src/main/java/member/controller/Login_mypage.java
@@ -0,0 +1,46 @@
+package member.controller;
+
+import common.controller.AbstractController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import member.domain.MemberVO;
+
+public class Login_mypage extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// === 로그인 유무 검사하기 === //
+			
+			if(!super.checkLogin(request) ) {
+		         // 로그인 하지 않은 경우이라면
+		         
+		         request.setAttribute("message", "비정상적인 경로로 들어왔습니다");
+		         request.setAttribute("loc", "javascript:history.back()"); 
+		         
+		      //   super.setRedirect(false);
+		         super.setViewPage("/WEB-INF/msg.jsp");
+		         return; // 종료
+		    }
+			
+			
+		    else { // 로그인 한 경우이라면 
+		    	  
+				HttpSession session = request.getSession();
+				MemberVO loginuser = (MemberVO) session.getAttribute("loginuser");		
+				
+				request.setAttribute("loginuser", loginuser);
+				
+				System.out.println("loginuser ==> " + loginuser.getPk_userid());
+				
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/myPage/login_myPage.jsp");
+				
+
+		    }
+			
+			
+
+	}
+
+}
